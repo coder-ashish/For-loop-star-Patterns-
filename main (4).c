39edc9009563ab665564ad12b1cb529a7c8d1817
Debashish jena <92994637+coder-ashish@users.noreply.github.com>
2021-10-28 20:34:46 +0530

    c compiler

---
+++ main (4).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int no=1;
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n-i;j++){
+            printf("  ");
+        }
+        for (int j=1;j<=i;j++){
+            printf("%d   ",i);
+        } 
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
